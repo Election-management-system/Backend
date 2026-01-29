ed37a13548a959160da7a606474b1ce43f1b8bfc
shubham-shinde-1800 <shubhamdshinde1800@gmail.com>
2026-01-29 20:24:03 +0530

    controller for email service added

---
+++ SmartVote-Email Service/EmailService/EmailService/Controllers/EmailController.cs
@@ -0,0 +1,25 @@
+﻿using EmailService.DTOs;
+using EmailService.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailService.Controllers
+{
+    [ApiController]
+    [Route("api/email")]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost("send")]
+        public IActionResult SendEmail([FromBody] EmailRequestDto request)
+        {
+            _emailService.SendEmail(request);
+            return Ok("Email sent successfully");
+        }
+    }
+}
