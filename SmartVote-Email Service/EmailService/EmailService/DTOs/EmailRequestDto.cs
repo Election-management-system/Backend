3e5df4486c229c6bc05bd5945cb6c721c4869d62
shubham-shinde-1800 <shubhamdshinde1800@gmail.com>
2026-01-30 20:28:14 +0530

    DTOS created for reciving data from spring API

---
+++ SmartVote-Email Service/EmailService/EmailService/DTOs/EmailRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace EmailService.DTOs
+{
+    public class EmailRequestDto
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
